822ebf5c50f3bba7637160fcecedb63335c53f34
zhengmianmian <zmianmian8@gmail.com>
2025-03-14 17:14:10 +1300

    added footer

--- movietruck.client/src/components/footer/Footer.tsx
+++ movietruck.client/src/components/footer/Footer.tsx
@@ -1,10 +1,36 @@
-import React from "react";
+import { Stack, Typography } from "@mui/material";
+import { FC } from "react";
 
-const Footer: React.FC = () => {
+const Footer: FC = () => {
+  const links = [
+    "Privacy",
+    "Customer Service",
+    "FAQ",
+    "Gift shop",
+    "Terms of use",
+  ];
   return (
-    <div>
-      <div>Footer</div>
-    </div>
+    <Stack
+      direction="row"
+      gap={4}
+      mt={2}
+      p={4}
+      sx={{
+        bgcolor: "black",
+        color: "white",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <Typography variant="h3">MovieTruck</Typography>
+      <Typography variant="h6">2025 - MTK Cinemas</Typography>
+      <Stack direction="row" gap={2}>
+        {links.map((link) => (
+          <Typography variant="body1">{link}</Typography>
+        ))}
+      </Stack>
+    </Stack>
   );
 };
 
